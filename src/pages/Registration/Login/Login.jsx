574fabc18ce01da884f6639ec1a85f011fe5d0e3
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-01-17 23:52:08 +0600

    design registration form

--- src/pages/Registration/Login/Login.jsx
+++ src/pages/Registration/Login/Login.jsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import GoogleButton from "../../../components/GoogleButton";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className=" w-full sm:max-w-6xl mx-auto p-2 sm:p-10">
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <div className="flex flex-col md:flex-row items-center justify-center shadow-2xl shadow-slate-800  my-8">
+        <div className=" w-full sm:w-3/4 mx-auto rounded-md">
+          <img
+            src="https://cdn-production.checkfront.com/wp-content/uploads/2021/07/marketing-tours-online-with-woman-traveling-by-boat-in-thailand.jpeg"
+            alt="login image"
+            className=" h-full sm:h-[600px]"
+          />
+        </div>
+        <div className="w-full sm:w-3/4 mx-auto text-center">
+          <h1 className="text-4xl font-semibold text-green-600">
+            Welcome Back
+          </h1>
+          <p className="text-2xl font-bold my-2">Please LogIn</p>
+          <form className="p-4">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="enter Your Email"
+              className="px-4 py-3 rounded-lg mt-5 w-full   focus:outline-none focus:bg-slate-500 font-semibold "
+            />
+            <input
+              type="password"
+              name="password"
+              required
+              id="password"
+              placeholder="password (6 min,one number,one capital letter,one special character)"
+              className="px-4 py-3 rounded-lg mt-5 w-full   focus:outline-none focus:bg-slate-500 text-xs "
+            />
+            <input
+              type="submit"
+              value="Login"
+              className="w-full mx-auto font-bold my-6 px-2 py-4 rounded-lg uppercase bg-slate-900 hover:bg-slate-600"
+            />
+            <GoogleButton />
+          </form>
+          <div>
+            <p>
+              Not registered Yet?
+              <Link className="ml-2 text-blue-700" to="/signup">
+                Signup
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Registration/Signup/SignUp.jsx
+++ src/pages/Registration/Signup/SignUp.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import GoogleButton from "../../../components/GoogleButton";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div className="max-w-6xl mx-auto p-10">
+    <div className="w-full sm:max-w-6xl mx-auto p-2 sm:p-10">
       <Helmet>
         <title>Sign Up </title>
       </Helmet>
-      <div className=" shadow-2xl flex items-center  justify-evenly gap-10 ">
-        <div className="w-3/4 rounded-lg  ">
+      <div className=" shadow-2xl flex flex-col  sm:flex-row items-center justify-evenly sm:gap-10 ">
+        <div className="w-full sm:w-3/4 rounded-lg  ">
           <img
             className="w-full mx-auto sm:h-[500px] "
             src="https://i.ibb.co/4Z0mL1R/login.jpg"
             alt="loginimg"
           />
         </div>
-        <div className="w-2/3  ">
-          <h1 className="text-4xl font-bold text-center">Please Sign Up</h1>
-          <form className="p-6 my-4">
+        <div className="w-full sm:w-2/3  ">
+          <h1 className=" text-2xl sm:text-4xl font-bold text-center">
+            Please Sign Up
+          </h1>
+          <form className="p-2 sm:p-6 my-4">
             <input
               type="text"
               required
@@ -56,6 +59,14 @@ export default function SignUp() {
             />
             <GoogleButton />
           </form>
+          <div className="text-center my-2 p-2">
+            <p className="text-xs  sm:text-xl font-semibold">
+              already have an account?{" "}
+              <Link className="text-blue-600" to="/login">
+                Login
+              </Link>{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
